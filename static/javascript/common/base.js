b592f6203a2eac56d081bffff24dfe835f7f7817
rio319-an <78468001+rio319-an@users.noreply.github.com>
2023-06-29 00:04:31 +0900

    Fix : github pages용 경로 재수정

--- static/javascript/common/base.js
+++ static/javascript/common/base.js
@@ -1,5 +1,5 @@
 const navigation = document.getElementById('navigation');
 
-fetch('/static/page/navigation.html')
+fetch('../../static/page/navigation.html')
     .then(res => res.text())
     .then(data => navigation.innerHTML = data);
