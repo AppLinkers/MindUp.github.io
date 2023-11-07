c644fcb41f6d916b857171bacf5a89c90b9c4ab1
안승우 <78468001+SeungWoo-Ahn@users.noreply.github.com>
2023-11-07 10:40:59 +0900

    Feat: 컨설턴트 프로필 탭 동작

--- static/javascript/consultant/profile.js
+++ static/javascript/consultant/profile.js
@@ -1,4 +1,6 @@
 const tabs = document.querySelectorAll(".label p");
+const teamCalendar = document.getElementById("calendar-team");
+const personalCalendar = document.getElementById("calendar-personal");
 
 tabs.forEach((tab, idx) => {
   tab.addEventListener("click", () => {
@@ -6,5 +8,12 @@ tabs.forEach((tab, idx) => {
       label.classList.remove("active");
     });
     tabs[idx].classList.add("active");
+    if (idx === 0) {
+      teamCalendar.style.display = "block";
+      personalCalendar.style.display = "none";
+    } else {
+      teamCalendar.style.display = "none";
+      personalCalendar.style.display = "block";
+    }
   });
 });
