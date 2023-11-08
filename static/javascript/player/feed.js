e9fa45f96cb9afb3caede1b856e9431cc040ccc8
안승우 <78468001+SeungWoo-Ahn@users.noreply.github.com>
2023-11-08 16:27:42 +0900

    Feat: 피드 상세화면 추가

--- static/javascript/player/feed.js
+++ static/javascript/player/feed.js
@@ -1,6 +1,7 @@
-const formatDateString = (dateStr) => {
-  const inputDate = new Date(dateStr);
-  const today = new Date();
+const today = new Date();
+
+const formatDateString = (dateString) => {
+  const inputDate = new Date(dateString);
   const optionsWithoutYear = { month: "long", day: "numeric" };
   const optionsWithYear = { year: "numeric", month: "long", day: "numeric" };
   if (inputDate.getFullYear() === today.getFullYear()) {
@@ -10,9 +11,33 @@ const formatDateString = (dateStr) => {
   }
 };
 
-const feeds = document.querySelectorAll(".feed-item");
+const formatCommentDateString = (dateString) => {
+  today.setHours(0, 0, 0, 0);
+  const date = new Date(dateString);
+  const timeDiff = today - date;
+  const oneDay = 24 * 60 * 60 * 1000;
+  const daysDiff = Math.round(timeDiff / oneDay);
+  if (daysDiff === 0) {
+    return "오늘";
+  } else if (daysDiff > 0 && daysDiff <= 31) {
+    return `${daysDiff}일 전`;
+  } else {
+    return formatDateString(dateString);
+  }
+};
 
-feeds.forEach((feed) => {
-  const date = feed.querySelector(".feed-date");
-  date.innerText = formatDateString(date.innerText);
+document.addEventListener("DOMContentLoaded", function () {
+  const feeds = document.querySelectorAll(".feed-item");
+
+  feeds.forEach((feed) => {
+    const date = feed.querySelector(".feed-date");
+    date.innerText = formatDateString(date.innerText);
+  });
+
+  const comments = document.querySelectorAll(".comments-list .comment-item");
+
+  comments.forEach((comment) => {
+    const date = comment.querySelector(".comment-date");
+    date.innerText = formatCommentDateString(date.innerText);
+  });
 });
