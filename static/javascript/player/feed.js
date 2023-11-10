cb38b210fcdf9c80236bdf7619b44530a114b9a2
안승우 <78468001+SeungWoo-Ahn@users.noreply.github.com>
2023-11-10 10:49:03 +0900

    Feat: 피드, 댓글 다이얼로그 추가

--- static/javascript/player/feed.js
+++ static/javascript/player/feed.js
@@ -28,16 +28,41 @@ const formatCommentDateString = (dateString) => {
 
 document.addEventListener("DOMContentLoaded", function () {
   const feeds = document.querySelectorAll(".feed-item");
+  const feedDialog = document.getElementById("feed-dialog");
 
   feeds.forEach((feed) => {
     const date = feed.querySelector(".feed-date");
     date.innerText = formatDateString(date.innerText);
+
+    const menu = feed.querySelector(".feed-menu");
+    menu.addEventListener("click", () => {
+      feedDialog.classList.add("active");
+    });
+  });
+
+  feedDialog.addEventListener("click", (event) => {
+    const dialogContent = feedDialog.querySelector(".dialog");
+    if (!dialogContent.contains(event.target)) {
+      feedDialog.classList.remove("active");
+    }
   });
 
   const comments = document.querySelectorAll(".comments-list .comment-item");
-
+  const commentDialog = document.getElementById("comment-dialog");
   comments.forEach((comment) => {
     const date = comment.querySelector(".comment-date");
     date.innerText = formatCommentDateString(date.innerText);
+
+    const menu = comment.querySelector(".comment-menu");
+    menu.addEventListener("click", () => {
+      commentDialog.classList.add("active");
+    });
+  });
+
+  commentDialog.addEventListener("click", (event) => {
+    const dialogContent = commentDialog.querySelector(".dialog");
+    if (!dialogContent.contains(event.target)) {
+      commentDialog.classList.remove("active");
+    }
   });
 });
